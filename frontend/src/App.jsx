04294d0c45b65279a154fbb71ee06fd4e07c49fd
Jacques Maarek <jack@MacBook-Pro.local>
2025-09-13 00:45:07 +0200

    fix: Forcer le bouton filtre à rester carré 32x32px

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -268,7 +268,7 @@ function App() {
               onClick={() => setShowFilterModal(true)}
               title="Filtres"
             >
-              <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M22 3H2l8 9.46V19l4 2v-8.54L22 3z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
               </svg>
             </button>
