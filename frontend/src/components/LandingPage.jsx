bd5c9af523c5904ec9d052a82ace278a9becf235
Jacques Maarek <jack@MacBook-Pro.local>
2025-09-13 18:04:05 +0200

    feat: Remplacer le texte par des icônes SVG dans la table de comparaison
    🎨 Nouvelles icônes SVG :
    - Icônes de coche dorées (#d4af8c) pour les 'Oui' de Playzio
    - Icônes de croix grises (#6b7280) pour les 'Non' et 'Partiel'
    - Icônes SVG de 20x20px pour un rendu net
    
    ✨ Améliorations visuelles :
    - Centrage parfait des icônes dans les cellules
    - Alignement vertical et horizontal optimal
    - Design plus moderne et professionnel
    - Couleurs cohérentes avec l'identité Playzio
    
    ✅ Résultat : Table de comparaison avec des icônes visuellement attractives

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -263,39 +263,111 @@ const LandingPage = ({ onLogin, onRegister }) => {
               <tbody>
                 <tr>
                   <td className="comparison-td feature-cell">Partager une dispo sans message</td>
-                  <td className="comparison-td success-cell">Oui</td>
-                  <td className="comparison-td error-cell">Non</td>
-                  <td className="comparison-td error-cell">Non</td>
+                  <td className="comparison-td success-cell">
+                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41L9 16.17z" fill="#d4af8c"/>
+                    </svg>
+                  </td>
+                  <td className="comparison-td error-cell">
+                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" fill="#6b7280"/>
+                    </svg>
+                  </td>
+                  <td className="comparison-td error-cell">
+                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" fill="#6b7280"/>
+                    </svg>
+                  </td>
                 </tr>
                 <tr>
                   <td className="comparison-td feature-cell">Voir les dispos de mes amis</td>
-                  <td className="comparison-td success-cell">Oui</td>
-                  <td className="comparison-td error-cell">Non</td>
-                  <td className="comparison-td error-cell">Non</td>
+                  <td className="comparison-td success-cell">
+                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41L9 16.17z" fill="#d4af8c"/>
+                    </svg>
+                  </td>
+                  <td className="comparison-td error-cell">
+                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" fill="#6b7280"/>
+                    </svg>
+                  </td>
+                  <td className="comparison-td error-cell">
+                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" fill="#6b7280"/>
+                    </svg>
+                  </td>
                 </tr>
                 <tr>
                   <td className="comparison-td feature-cell">Rejoindre une sortie en un clic</td>
-                  <td className="comparison-td success-cell">Oui</td>
-                  <td className="comparison-td error-cell">Non</td>
-                  <td className="comparison-td error-cell">Non</td>
+                  <td className="comparison-td success-cell">
+                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41L9 16.17z" fill="#d4af8c"/>
+                    </svg>
+                  </td>
+                  <td className="comparison-td error-cell">
+                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" fill="#6b7280"/>
+                    </svg>
+                  </td>
+                  <td className="comparison-td error-cell">
+                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" fill="#6b7280"/>
+                    </svg>
+                  </td>
                 </tr>
                 <tr>
                   <td className="comparison-td feature-cell">Pas besoin de groupe dédié</td>
-                  <td className="comparison-td success-cell">Oui</td>
-                  <td className="comparison-td error-cell">Non</td>
-                  <td className="comparison-td error-cell">Non</td>
+                  <td className="comparison-td success-cell">
+                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41L9 16.17z" fill="#d4af8c"/>
+                    </svg>
+                  </td>
+                  <td className="comparison-td error-cell">
+                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" fill="#6b7280"/>
+                    </svg>
+                  </td>
+                  <td className="comparison-td error-cell">
+                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" fill="#6b7280"/>
+                    </svg>
+                  </td>
                 </tr>
                 <tr>
                   <td className="comparison-td feature-cell">Pas d'engagement figé</td>
-                  <td className="comparison-td success-cell">Oui</td>
-                  <td className="comparison-td warning-cell">Partiel</td>
-                  <td className="comparison-td error-cell">Non</td>
+                  <td className="comparison-td success-cell">
+                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41L9 16.17z" fill="#d4af8c"/>
+                    </svg>
+                  </td>
+                  <td className="comparison-td error-cell">
+                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" fill="#6b7280"/>
+                    </svg>
+                  </td>
+                  <td className="comparison-td error-cell">
+                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" fill="#6b7280"/>
+                    </svg>
+                  </td>
                 </tr>
                 <tr>
                   <td className="comparison-td feature-cell">Accessible web + mobile facilement</td>
-                  <td className="comparison-td success-cell">Oui</td>
-                  <td className="comparison-td warning-cell">Partiel</td>
-                  <td className="comparison-td warning-cell">Partiel</td>
+                  <td className="comparison-td success-cell">
+                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41L9 16.17z" fill="#d4af8c"/>
+                    </svg>
+                  </td>
+                  <td className="comparison-td error-cell">
+                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" fill="#6b7280"/>
+                    </svg>
+                  </td>
+                  <td className="comparison-td error-cell">
+                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" fill="#6b7280"/>
+                    </svg>
+                  </td>
                 </tr>
               </tbody>
             </table>
