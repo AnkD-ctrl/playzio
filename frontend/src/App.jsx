22ae44f0d3b42fbaf615b33605c0760fc79c15d0
Jacques Maarek <jack@MacBook-Pro.local>
2025-09-21 22:01:27 +0200

    📝 Changement 'Publiques' en 'Dispos publiques'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -344,7 +344,7 @@ function App() {
                     <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                   </svg>
                 </span>
-                <span>Publiques</span>
+                <span>Dispos publiques</span>
               </div>
             </div>
           </div>
